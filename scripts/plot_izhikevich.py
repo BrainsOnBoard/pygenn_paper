5776b345078d862b04d86c0bafba83d556dea136
neworderofjamie <neworderofjamie@gmail.com>
2021-03-11 17:59:23 +0000

    removed content on GPU stimulus delivery

--- scripts/plot_izhikevich.py
+++ scripts/plot_izhikevich.py
@@ -8,8 +8,7 @@ from six import iterkeys, itervalues
 
 # Names and algorithms - could extract them from CSV but it's a ball-ache
 devices = ["GeForce\nGTX 1650\nLinux", "Jetson\nXavier NX\nLinux", "Titan\nRTX\nLinux", "Geforce\nGTX 1050 Ti\nWindows"]
-algorithms = ["Python", "Python: GPU stim", "Python: GPU recording",
-              "Python: GPU stim & recording", "C++: GPU stim & recording", ]
+algorithms = ["Python", "Python: GPU recording", "C++: GPU recording"]
 
 # Import data
 # **NOTE** np.loadtxt doesn't handle empty entries
@@ -41,8 +40,8 @@ fig, axis = plt.subplots(figsize=(plot_settings.column_width,
 group_x = []
 for d in range(0, num_bars, group_size):
     end = start + ((bar_width + bar_pad) * group_size)
-    bar_x[d:d + group_size] = np.arange(start, end, bar_width + bar_pad)
-
+    bar_x[d:d + group_size] = np.arange(start, end - 0.1, bar_width + bar_pad)
+    
     group_x.append(start + ((end - bar_width - start) * 0.5))
 
     # Update start for next group
@@ -52,7 +51,7 @@ pal = sns.color_palette("deep")
 legend_actors = []
 for i, a in enumerate(algorithms):
     bars = axis.bar(bar_x[i::group_size], time_mean[i::group_size] / 1000.0, 
-    bar_width, color=pal[i], linewidth=0, yerr=time_std[i::group_size] / 1000.0)
+                    bar_width, color=pal[i], linewidth=0)#, yerr=time_std[i::group_size] / 1000.0)
     legend_actors.append(bars[0])
 
 # Configure axis
@@ -65,10 +64,10 @@ sns.despine(ax=axis)
 axis.xaxis.grid(False)
 
 # Show figure legend with devices beneath figure
-fig.legend(legend_actors, algorithms, ncol=2,
+fig.legend(legend_actors, algorithms, ncol=3, columnspacing=1.5,
            frameon=False, loc="lower center")
 
-plt.tight_layout(pad=0, rect=[0.0, 0.175, 1.0, 1.0])
+plt.tight_layout(pad=0, rect=[0.0, 0.1, 1.0, 1.0])
 if not plot_settings.presentation:
     fig.savefig("../figures/izhikevich.eps")
 plt.show()
