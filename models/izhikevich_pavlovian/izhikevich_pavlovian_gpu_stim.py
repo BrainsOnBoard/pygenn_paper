e0a0fd242d6481120294a3adf381c65d5f1ced53
neworderofjamie <neworderofjamie@gmail.com>
2022-06-07 13:59:20 +0100

    bad default

--- models/izhikevich_pavlovian/izhikevich_pavlovian_gpu_stim.py
+++ models/izhikevich_pavlovian/izhikevich_pavlovian_gpu_stim.py
@@ -42,7 +42,7 @@ stim_noise_model = genn_model.create_custom_current_source_class(
 # Stimuli generation
 # ----------------------------------------------------------------------------
 # Get standard model parameters
-params = get_params(build_model=False, measure_timing=False, use_genn_recording=True)
+params = get_params(build_model=True, measure_timing=False, use_genn_recording=True)
 params["seed"] = 1234
 if "seed" in params:
     np.random.seed(params["seed"])
